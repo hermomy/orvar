8852e0727340d279c45b80ff4172826b53a16c5f
Oska <oskang09@Oskas-Mac-mini.local>
2019-03-22 17:41:47 +0800

    #C StatefulLink

---
+++ app/components/StatefulLink/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import StatefulLink from '../index';
+
+describe('<StatefulLink />', () => {
+    it('Expect to have unit tests specified', () => {
+        expect(true).toEqual(false);
+    });
+});
