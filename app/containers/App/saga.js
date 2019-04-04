fca81ccd4a063b15d956266df5c088c0383dd30c
Oskang09 <developer.oskang@gmail.com>
2019-04-04 11:45:24 +0800

    Update for global common config fetch

--- app/containers/App/saga.js
+++ app/containers/App/saga.js
@@ -1,5 +1,6 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
-import { staticErrorResponse, request } from 'globalUtils';
+import { staticErrorResponse, apiRequest } from 'globalUtils';
+import globalScope from 'globalScope';
 
 import {
     FETCH_CONFIG,
@@ -19,11 +20,12 @@ export function* getConfigData() {
     let err;
 
     try { // Trying the HTTP Request
-        const response = yield call(request, API.URL, API.PARAMS);
-        if (response && response.success !== false) {
-            yield put(fetchConfigSuccess(response));
-        } else if (response && response.success === false) {
-            yield put(fetchConfigFailed(response));
+        const response = yield call(apiRequest, API.URL, API.PARAMS);
+        if (response && response.ok !== false) {
+            globalScope.config = response.data;
+            yield put(fetchConfigSuccess(response.data));
+        } else if (response && response.ok === false) {
+            yield put(fetchConfigFailed(response.data));
         } else {
             err = staticErrorResponse({ text: 'No response from server' });
             throw err;
