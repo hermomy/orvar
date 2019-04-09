a6a317755d6012a56cb4850039a72ed71d5f6977
TienPing <petwesley@gmail.com>
2019-04-09 21:20:55 +0800

    remove unused container

--- app/containers/MallPage/tests/reducer.test.js
+++
@@ -1,9 +0,0 @@
-
-import { fromJS } from 'immutable';
-import mallPageReducer from '../reducer';
-
-describe('mallPageReducer', () => {
-    it('returns the initial state', () => {
-        expect(mallPageReducer(undefined, {})).toEqual(fromJS({}));
-    });
-});
