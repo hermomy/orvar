2f130f0d91c335ec0acffdfd18f8f68473beaf9d
lee wang lin <leewanglin@lees-Mac-mini.local>
2019-04-12 09:54:50 +0800

    profile page test

--- app/containers/HerListing/tests/selectors.test.js
+++ app/containers/HerListing/tests/selectors.test.js
@@ -1,6 +1,6 @@
-import { fromJS } from 'immutable';
-        const selector = selectHerListingDomain();
-        const mock = fromJS({ herListing: initialState });
-        expect(selector(mock)).toEqual(initialState.toJS());
-    });
-});
+// import { fromJS } from 'immutable';
+//         const selector = selectHerListingDomain();
+//         const mock = fromJS({ herListing: initialState });
+//         expect(selector(mock)).toEqual(initialState.toJS());
+//     });
+// });
