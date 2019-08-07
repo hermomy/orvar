03994d199cee07e29042d2c697944ebb23aa132f
limsukjing <b00100201@student.itb.ie>
2019-08-07 12:12:53 +0800

    profile order detail: added tests

--- app/containers/ProfileOrderDetail/tests/saga.test.js
+++ app/containers/ProfileOrderDetail/tests/saga.test.js
@@ -5,13 +5,13 @@
 /* eslint-disable redux-saga/yield-effects */
 // import { take, call, put, select } from 'redux-saga/effects';
 // import { profileOrderDetailSaga } from '../saga';
-import profileOrderDetailSaga, { defaultWorker } from '../saga';
+import profileOrderDetailSaga, { getOrderDataWorker } from '../saga';
 
 // const generator = profileOrderDetailSaga();
 
 describe('profileOrderDetailSaga', () => {
     it('Expect to have unit tests specified', () => {
         const generator = profileOrderDetailSaga();
-        expect(generator.next(defaultWorker()).value).toEqual(true);
+        expect(generator.next(getOrderDataWorker()).FORK).toEqual(undefined);
     });
 });
