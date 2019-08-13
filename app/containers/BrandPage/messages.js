166ea908b5d5e94253e544d12b5fbf2b85d6d257
Muhammad Akif Farhan <38977468+akifarhan@users.noreply.github.com>
2019-08-13 14:11:01 +0800

    Brand list page created (#64)
    * initiate brand page container and create filter component
    
    * fix selector test generator that cause eslint error
    
    * homepage revamp using material ui and compile hardcoded content into array
    
    * brand page initiate
    
    * added brandList
    
    * updated space
    
    * fixed errors and update layout
    
    * filter scrolled
    
    * installed react-scroll
    
    * updated breakpoints according to MaterialUI, fixed scroll and several components to be responsive

---
+++ app/containers/BrandPage/messages.js
@@ -0,0 +1,13 @@
+/*
+ * BrandPage Messages
+ *
+ * This contains all the text for the BrandPage component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+    header: {
+        id: 'app.containers.BrandPage.header',
+        defaultMessage: 'This is BrandPage container !',
+    },
+});
