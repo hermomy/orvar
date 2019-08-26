fc5e0d76e015ca99c52e33a617d8ff7156a108d0
limsukjing <b00100201@student.itb.ie>
2019-08-26 10:40:11 +0800

    profile wishlist: added testing

--- app/containers/ProfileWishlist/tests/saga.test.js
+++ app/containers/ProfileWishlist/tests/saga.test.js
@@ -5,13 +5,13 @@
 /* eslint-disable redux-saga/yield-effects */
 // import { take, call, put, select } from 'redux-saga/effects';
 // import { profileWishlistSaga } from '../saga';
-import profileWishlistSaga, { WishlistDataWorker } from '../saga';
+import profileWishlistSaga, { getWishlistWorker } from '../saga';
 
 // const generator = profileWishlistSaga();
 
 describe('profileWishlistSaga', () => {
     it('Expect to have unit tests specified', () => {
         const generator = profileWishlistSaga();
-        expect(generator.next(WishlistDataWorker()).FORK).toEqual(undefined);
+        expect(generator.next(getWishlistWorker()).FORK).toEqual(undefined);
     });
 });
