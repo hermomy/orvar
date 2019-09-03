61ddbb18b575f5daef4d2efeb086ef477c849a36
Akif <akifarhan@gmail.com>
2019-09-03 09:08:47 +0800

    Added function for redeemRewards and getRewardsInfo. Need update on message

--- app/containers/ProfileRewards/index.js
+++ app/containers/ProfileRewards/index.js
@@ -21,13 +21,17 @@ import {
     Card,
     CardHeader,
     CardContent,
+    CardActions,
     Avatar,
+    ButtonBase,
 } from '@material-ui/core';
+import PopupDialog from 'components/PopupDialog';
+import parse from 'html-react-parser';
 import {
     getRewards,
+    getRewardsInfo,
+    redeemRewards,
 } from './actions';
-
-
 import makeSelectProfileRewards from './selectors';
 import reducer from './reducer';
 import saga from './saga';
@@ -38,41 +42,139 @@ export class ProfileRewards extends React.PureComponent {
         super(props);
         this.state = {
             type: 'locals',
+            popup: false,
+            clickRedeem: false,
         };
     }
     componentDidMount() {
         this.props.dispatch(getRewards({ type: this.state.type }));
     }
-    renderRewards = () => dataChecking(this.props, 'profileRewards', 'data', 'data', 'items') &&
-        this.props.profileRewards.data.data.items.map((item, index) => (
+    componentWillReceiveProps(nextProps) {
+        if (this.props.profileRewards.redeemRewards.success !== nextProps.profileRewards.redeemRewards.success && nextProps.profileRewards.redeemRewards.success) {
+            this.setState({ clickRedeem: false });
+        }
+    }
+    renderRewards = () => dataChecking(this.props, 'profileRewards', 'rewards', 'data', 'data', 'items') &&
+        this.props.profileRewards.rewards.data.data.items.map((item, index) => (
             <Grid item={true} key={index} xs={12} sm={6} md={4}>
                 <Card id={`reward-item ${index}`} className="my-1">
-                    <CardHeader
-                        title={
-                            <Typography>{item.partner.name}</Typography>
-                        }
-                        avatar={
-                            <Avatar>
-                                <img width="110%" alt="partner-logo" src={item.partner.logo} />
-                            </Avatar>
-                        }
-                        img={<img width="100%" alt="reward-items" src={item.banner} />}
-                    />
-                    <CardContent>
-                        <Typography variant="body2" color="primary">{item.value.amount} {item.value.currency}</Typography>
-                        <br />
-                        <Typography variant="body1">{item.title}</Typography>
-                        <br />
-                        <Typography variant="caption">{item.remark}</Typography>
-                    </CardContent>
+                    <ButtonBase
+                        onClick={() => {
+                            this.props.dispatch(getRewardsInfo(item._weblink.params.url));
+                            this.setState({ popup: true });
+                        }}
+                    >
+                        <CardHeader
+                            title={
+                                <Typography>{item.partner.name}</Typography>
+                            }
+                            avatar={
+                                <Avatar>
+                                    <img width="110%" alt="partner-logo" src={item.partner.logo} />
+                                </Avatar>
+                            }
+                        />
+                        <CardContent>
+                            <Typography variant="body2" color="primary">{item.value.amount} {item.value.currency}</Typography>
+                            <br />
+                            <Typography variant="body1">{item.title}</Typography>
+                            <br />
+                            <Typography variant="caption">{item.remark}</Typography>
+                        </CardContent>
+                    </ButtonBase>
                 </Card>
             </Grid>
         )
     )
     renderAccountBalance = () => {
-        if (dataChecking(this.props, 'profileRewards', 'data', 'data', 'balance', 'amount')) {
+        if (dataChecking(this.props, 'profileRewards', 'rewards', 'data', 'data', 'balance')) {
             return (
-                <Typography color="secondary">{this.props.profileRewards.data.data.balance.amount}</Typography>
+                <Typography color="secondary">{this.props.profileRewards.rewards.data.data.balance.amount}</Typography>
+            );
+        }
+        return null;
+    }
+    renderDialogContent = () => {
+        if (dataChecking(this.props, 'profileRewards', 'rewardsInfo', 'data')) {
+            const info = this.props.profileRewards.rewardsInfo.data.data;
+            return (
+                <div>
+                    <CardHeader
+                        title={
+                            <Typography>{info.partner.name}</Typography>
+                        }
+                        avatar={
+                            <Avatar>
+                                <img width="110%" alt="partner-logo" src={info.partner.logo} />
+                            </Avatar>
+                        }
+                        subheader={
+                            <Typography variant="caption" color="primary"><br />{info.value.amount} {info.value.currency}</Typography>
+                        }
+                    />
+                    <CardContent>
+                        <Typography>{info.title}</Typography>
+                    </CardContent>
+                    <CardActions>
+                        {this.state.clickRedeem ?
+                            <Container>
+                                {/* CLICK */}
+                                <Typography variant="h6" color="primary">Redeem Now</Typography>
+                                <br />
+                                <Typography>You will be deducted <b>{info.value.amount} {info.value.currency}</b> in order to redeem the voucher.</Typography>
+                                <Button variant="outlined" onClick={() => this.setState({ clickRedeem: false })}>No</Button>
+                                <Button
+                                    variant="outlined"
+                                    color="secondary"
+                                    onClick={() => {
+                                        if (this.state.type === 'locals') {
+                                            this.props.dispatch(redeemRewards(info._weblink.params.url, 'locals'));
+                                        } else {
+                                            this.props.dispatch(redeemRewards(info._weblink.params.url, null));
+                                        }
+                                    }}
+                                >
+                                Yes
+                                </Button>
+                            </Container>
+                            :
+                            <div>
+                                {
+                                    dataChecking(this.props, 'profileRewards', 'redeemRewards', 'data')
+                                    ?
+                                        <div>
+                                            <Typography variant="h5">{this.props.profileRewards.redeemRewards.data.data.message.title}</Typography>
+                                            <br />
+                                            <Typography>{this.props.profileRewards.redeemRewards.data.data.message.content}</Typography>
+                                        </div>
+                                    :
+                                        <Button
+                                            variant="outlined"
+                                            color="secondary"
+                                            fullWidth={true}
+                                            onClick={() => this.setState({ clickRedeem: true })}
+                                        >
+                                        Redeem Now
+                                        </Button>
+                                }
+                            </div>
+                        }
+                    </CardActions>
+                    <CardContent>
+                        <Typography variant="h5">Terms and Conditions</Typography>
+                        <br />
+                        {parse(info.tnc)}
+                    </CardContent>
+                    { info.partner.description ?
+                        <CardContent>
+                            <Typography variant="h5">About {info.partner.name}</Typography>
+                            <br />
+                            <Typography>{info.partner.description}</Typography>
+                        </CardContent>
+                        :
+                        null
+                    }
+                </div>
             );
         }
         return null;
@@ -105,6 +207,15 @@ export class ProfileRewards extends React.PureComponent {
                 <Grid container={true} spacing={1}>
                     {this.renderRewards()}
                 </Grid>
+                <PopupDialog
+                    display={this.state.popup}
+                    title="Reward Details"
+                    onClose={() => {
+                        this.setState({ popup: false });
+                    }}
+                >
+                    {this.renderDialogContent()}
+                </PopupDialog>
             </Container>
         );
     }
--- app/containers/ProfileRewards/saga.js
+++ app/containers/ProfileRewards/saga.js
@@ -5,11 +5,17 @@ import {
 } from 'redux-saga/effects';
 import {
     GET_REWARDS,
+    GET_REWARDS_INFO,
+    REDEEM_REWARDS,
 } from './constants';
 import { apiRequest } from '../../globalUtils';
 import {
     getRewardsSuccess,
     getRewardsFail,
+    getRewardsInfoSuccess,
+    getRewardsInfoFail,
+    redeemRewardsSuccess,
+    redeemRewardsFail,
 } from './actions';
 
 export function* getRewardsQuery(action) {
@@ -20,8 +26,37 @@ export function* getRewardsQuery(action) {
         yield put(getRewardsFail(response.data));
     }
 }
+export function* getRewardsInfoQuery(action) {
+    const url = `/services/gami${action.url}`;
+    const response = yield call(apiRequest, url, 'get');
+    if (response && response.ok) {
+        yield put(getRewardsInfoSuccess(response.data));
+    } else {
+        yield put(getRewardsInfoFail(response.data));
+    }
+}
+export function* redeemRewardsQuery(action) {
+    const url = `/services/gami${action.url}`;
+    if (action.types === 'locals') {
+        const response = yield call(apiRequest, url, 'post');
+        if (response && response.ok) {
+            yield put(redeemRewardsSuccess(response.data));
+        } else {
+            yield put(redeemRewardsFail(response.data));
+        }
+    } else {
+        const response = yield call(apiRequest, url, 'put');
+        if (response && response.ok) {
+            yield put(redeemRewardsSuccess(response.data));
+        } else {
+            yield put(redeemRewardsFail(response.data));
+        }
+    }
+}
 
 // Individual exports for testing
 export default function* profileRewardsSaga() {
     yield takeLatest(GET_REWARDS, getRewardsQuery);
+    yield takeLatest(GET_REWARDS_INFO, getRewardsInfoQuery);
+    yield takeLatest(REDEEM_REWARDS, redeemRewardsQuery);
 }
