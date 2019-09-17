ae2a9d25ef566bc7c9f0580fd5261d0a37af1be7
pablo <akifarhan@gmail.com>
2019-09-17 17:42:52 +0800

    Created CartList component and applied to cartPage

---
+++ app/components/CartList/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import CartList from '../index';
+
+describe('<CartList />', () => {
+    it('Expect to have unit tests specified', () => {
+        expect(true).toEqual(false);
+    });
+});
