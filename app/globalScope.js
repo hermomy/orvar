16a3a9293b4810891eb4b181c265babf0cee9bc0
akifarhan <akifarhan@gmail.com>
2019-10-24 17:49:21 +0800

    Add fbId to globalScope

--- app/globalScope.js
+++ app/globalScope.js
@@ -2,6 +2,7 @@ const globalScope = {
     token: '',
     isAdmin: false,
     api: process.env.API_URL,
+    fb_id: process.env.FACEBOOK_APP_ID,
     previousPage: '',
     config: {},
     axios: null, // create in initialiseApp.js
